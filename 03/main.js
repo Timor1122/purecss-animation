a2ba0fe3f8b5a8a2bdad33dc8df641c174ce579c
Timor1122 <qq961108639@icloud.com>
2021-07-15 08:40:38 +0800

    hh

--- 03/main.js
+++ 03/main.js
@@ -1,26 +1,47 @@
 console.clear();
 let state = false;
-let move = [
+let moveX = [
     {
         target: "#arm_left",
-        position: [0, -400]
+        position: [0, -400],
+        coordinate: "x"
     },
 
     {
         target: "#arm_right",
-        position: [0, 400]
+        position: [0, 400],
+        coordinate: "x"
+    },
+]
+let moveY =
+    {
+        target: "#head",
+        position: [100, 0],
+        scale: [1, .9]
+    }
+let eyesAll = [
+    {
+        target: "#eye_left",
+        scale: [1, .5]
+    },
+    {
+        target: "#eye_right",
+        scale: [1, .5]
     }
 ]
-$('.robot').load('https://ax.minicg.com/temp/robot.svg', function (res) {
 
+$('.robot').load('robot.svg', function (res) {
+
+    eyes();
     $("#green_dot").click(function () {
         state = !state;
-        action()
+        actionX();
+        actionY();
+        scaleY();
     })
-
-});
-function action() {
-    $.each(move, function (i, k) {
+})
+function actionX() {
+    $.each(moveX, function (i, k) {
 
         let x = state ? k.position[0] : k.position[1];
         anime({
@@ -28,4 +49,21 @@ function action() {
             translateX: x
         })
     })
+}
+
+function actionY() {
+    let y = state ? moveY.position[0] : moveY.position[1];
+    anime({
+        targets: moveY.target,
+        translateY: y
+    })
+}
+
+function scaleY() {
+    let scaleY = state ? moveY.scale[0] : moveY.scale[1];
+    anime({
+        targets: moveY.target,
+        scaleY: scaleY
+    })
+
 }
